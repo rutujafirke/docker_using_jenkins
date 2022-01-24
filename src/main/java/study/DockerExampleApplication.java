d336baa270e449b7a64cbc9f7f222d68e21075ae
rutujafirke <rutujafirke123@gmail.com>
2022-01-24 16:40:31 +0530

    commit

---
+++ src/main/java/study/DockerExampleApplication.java
@@ -0,0 +1,15 @@
+package study;
+public class DockerExampleApplication {
+
+	public static void main(String[] args) {
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+		// Testing in the main method
+	}
+
+	public static boolean checkIfInputIsAnEvenNumber(int
+			number){
+		return number % 2 == 0;
+	}
+
+
+}
